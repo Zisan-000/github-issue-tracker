07c6ac143573c564e512c45ee85e8b10510e4701
Zisan-000 <zobaer.zisan@gmail.com>
2026-03-09 22:34:46 +0600

    rcheck

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@ const signIn = () => {
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
   if (username === "admin" && password === "admin123") {
-    window.location.href = "index.html";
+    window.location.href = "dashboard.html";
   } else {
     alert("Invalid username or password");
   }
